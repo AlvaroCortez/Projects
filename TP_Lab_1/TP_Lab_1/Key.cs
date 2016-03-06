f0007d721eb830bcf26918341db9d0da1e35a754
vladplakhov <vladplakhov@gmail.com>
2016-03-06 20:52:56 +0400

    Add methods in class Key

--- TP_Lab_1/TP_Lab_1/Key.cs
+++ TP_Lab_1/TP_Lab_1/Key.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TP_Lab_1
 {
+    
     class Key
     {
+        private RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider();
+        private String publicKey = null;
+        private String privateKey = null;
+        private static Key instance = null;
+
+        private Key()
+        {
+            publicKey = rsaKey.ToXmlString(false);
+            privateKey = rsaKey.ToXmlString(true);
+        }
+
+        public static Key GetInstance()
+        {
+            return instance ?? (instance = new Key());
+        }
+
+        public string PublicKey
+        {
+            get { return publicKey; }
+        }
+
+        public string PrivateKey
+        {
+            get { return privateKey; }
+        }
     }
 }
--- TP_Lab_1/TP_Lab_1/Program.cs
+++ TP_Lab_1/TP_Lab_1/Program.cs
@@ -10,6 +10,7 @@ namespace TP_Lab_1
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
